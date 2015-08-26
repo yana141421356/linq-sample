9c24e2f3bdc0cd2fc1c41202bfcdf2b3a78a7b88
ayanai <atsushi.yanai1977@gmail.com>
2015-08-27 00:29:35 +0900

    create new

---
+++ LinqCount/LinqCount/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> source = new List<int>() { 0, 1, 2, 3, 4 };
+            Console.WriteLine(source.Count());
+            Console.ReadLine();
+        }
+    }
+}
