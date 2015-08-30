dbba71ea7723a734387653d7aecec7f5c28af4cd
ayanai <atsushi.yanai1977@gmail.com>
2015-08-30 22:44:49 +0900

    chenged replaced with a real type

--- LinqCount/LinqCount/Program.cs
+++ LinqCount/LinqCount/Program.cs
@@ -10,9 +10,28 @@ namespace LinqCount
     {
         static void Main(string[] args)
         {
+            // http://smdn.jp/programming/netfx/tips/implement_linq/
             IEnumerable<int> source = new List<int>() { 0, 1, 2, 3, 4 };
-            Console.WriteLine(source.Count());
+            //Console.WriteLine(source.Count());
+            int count = 0;
+            foreach (var e in source)
+            {
+                count++;
+            }
+            Console.WriteLine(Count(source));
+            
             Console.ReadLine();
+        }
+
+        static int Count<TSource>(IEnumerable<TSource> source)
+        {
+            int count = 0;
+
+            foreach (var e in source)
+            {
+                count++;
+            }
+            return count;
         }
     }
 }
